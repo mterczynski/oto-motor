b00746a03cb78271414b73b582d3ff8db7c4e7b5
Michał Terczyński <mterczynski1@gmail.com>
2023-11-25 21:16:53 +0100

    Get offers from backend

---
+++ src/app/offers/route.ts
@@ -0,0 +1,39 @@
+import { MotorcycleSaleOffer } from "../types/motorcycle";
+
+export const dynamic = 'force-dynamic' // defaults to force-static
+export async function GET() {
+    // const res = await fetch('https://data.mongodb-api.com/...', {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'API-Key': process.env.DATA_API_KEY,
+    //   },
+    // })
+    // const data = await res.json()
+
+    // return Response.json({ data })
+
+    const data: MotorcycleSaleOffer[] = [
+        {
+            brand: "Yamaha",
+            description: "Witam, mam do sprzedania nowiutką Yamahę",
+            mileageInKm: 10000,
+            model: "MT-07",
+            priceInPLN: 30000,
+            imageLink: "/mt07.webp",
+            location: "Kraków",
+            distanceInKm: 5,
+        },
+        {
+            brand: "Kawasaki",
+            description: "Witam, mam do sprzedania nową Kawę",
+            mileageInKm: 20000,
+            model: "Versys 600",
+            priceInPLN: 34000,
+            imageLink: "/versys600.webp",
+            location: "Wieliczka",
+            distanceInKm: 15,
+        },
+    ];
+
+    return Response.json({ data })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,55 @@
 "use client";
-import { useState } from "react";
-import { MotorcycleSaleOffer } from "./types/motorcycle";
 import Image from "next/image";
-
-const mockOffers: MotorcycleSaleOffer[] = [
-  {
-    brand: "Yamaha",
-    description: "Witam, mam do sprzedania nowiutką Yamahę",
-    mileageInKm: 10000,
-    model: "MT-07",
-    priceInPLN: 30000,
-    imageLink: "/mt07.webp",
-    location: "Kraków",
-    distanceInKm: 5,
-  },
-  {
-    brand: "Kawasaki",
-    description: "Witam, mam do sprzedania nową Kawę",
-    mileageInKm: 20000,
-    model: "Versys 600",
-    priceInPLN: 34000,
-    imageLink: "/versys600.webp",
-    location: "Wieliczka",
-    distanceInKm: 15,
-  },
-];
+import { useState, useEffect } from "react";
+import { MotorcycleSaleOffer } from "./types/motorcycle";
 
 const formatNumber = (price: number) => {
   return price.toLocaleString();
 };
 
 export default function Home() {
-  const [saleOffers, setSaleOffers] = useState<MotorcycleSaleOffer[]>([
-    ...mockOffers,
-  ]);
+  const [saleOffers, setSaleOffers] = useState<MotorcycleSaleOffer[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/offers")
+      .then((res) => res.json())
+      .then((data) => {
+        setSaleOffers(data.data);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl">Oto Motor</h1>
 
-      <div className="flex">
-        {saleOffers.map((offer, key) => (
-          <div
-            className="flex flex-col mr-16 bg-neutral-200 text-black p-8 border-4"
-            key={key}
-          >
-            <Image alt="" src={offer.imageLink} width={200} height={200} />
-            <div>Cena: {formatNumber(offer.priceInPLN)} zł</div>
-            <div>Marka: {offer.brand}</div>
-            <div>Model: {offer.model}</div>
-            <div>
-              Lokacja: {offer.location}, {offer.distanceInKm} km
-            </div>
-            <div>Przebieg: {formatNumber(offer.mileageInKm)} km</div>
-            {/* <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="flex">
+          {saleOffers.map((offer, key) => (
+            <div
+              className="flex flex-col mr-16 bg-neutral-200 text-black p-8 border-4"
+              key={key}
+            >
+              <Image alt="" src={offer.imageLink} width={200} height={200} />
+              <div>Cena: {formatNumber(offer.priceInPLN)} zł</div>
+              <div>Marka: {offer.brand}</div>
+              <div>Model: {offer.model}</div>
+              <div>
+                Lokacja: {offer.location}, {offer.distanceInKm} km
+              </div>
+              <div>Przebieg: {formatNumber(offer.mileageInKm)} km</div>
+              {/* <div>
               Opis:
               <br />
               <p>{offer.description}</p>
             </div> */}
-          </div>
-        ))}
-      </div>
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
