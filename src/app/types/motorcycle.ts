70c57c9904812afb1c99d773a52eee6e0bb86306
Michał Terczyński <mterczynski1@gmail.com>
2023-11-25 16:59:38 +0100

    Add 2 offers

---
+++ src/app/types/motorcycle.ts
@@ -0,0 +1,8 @@
+export interface MotorcycleSaleOffer {
+    priceInPLN: number;
+    brand: string;
+    model: string;
+    mileageInKm: number;
+    description: string;
+    imageLink: string;
+}
