f19d36fd30ddae5c9200e8c5ea0249660d89c1dd
Michał Terczyński <mterczynski1@gmail.com>
2023-11-26 02:05:05 +0100

    Add more mock offers, add uuids to offers

--- src/app/types/motorcycle.ts
+++ src/app/types/motorcycle.ts
@@ -1,10 +1,11 @@
 export interface MotorcycleSaleOffer {
-    priceInPLN: number;
-    brand: string;
-    model: string;
-    mileageInKm: number;
-    description: string;
-    imageLink: string;
-    location: string;
-    distanceInKm: number;
+  id: string
+  priceInPLN: number;
+  brand: string;
+  model: string;
+  mileageInKm: number;
+  description: string;
+  imageLink: string;
+  location: string;
+  distanceInKm: number;
 }
