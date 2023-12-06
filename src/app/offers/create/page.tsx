45375912235167608771a015f7b5ab9311fcfbb8
Michał Terczyński <mterczynski1@gmail.com>
2023-12-06 23:49:41 +0100

    Style create form

--- src/app/offers/create/page.tsx
+++ src/app/offers/create/page.tsx
@@ -22,7 +22,7 @@ function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending}>
+    <button type="submit" className="bg-green-500 mt-4" aria-disabled={pending}>
       {pending ? "Submitting..." : "Submit"}
     </button>
   );
@@ -34,71 +34,36 @@ export default function AddForm() {
   });
 
   return (
-    <form action={formAction}>
+    <form action={formAction} className="flex flex-col">
       <label htmlFor="input-brand" className="text-white">
         Marka
       </label>
-      <input
-        className="text-black"
-        type="text"
-        id="input-brand"
-        name="brand"
-        required
-      />
-
-      <label htmlFor="input-mileageInKm" className="text-white">
-        Przebieg w km
-      </label>
-      <input
-        className="text-black"
-        type="number"
-        id="input-mileageInKm"
-        name="mileageInKm"
-        required
-      />
+      <input className="text-black" type="text" name="brand" required />
 
       <label htmlFor="input-model" className="text-white">
         Model
       </label>
-      <input
-        className="text-black"
-        type="text"
-        id="input-model"
-        name="model"
-        required
-      />
+      <input className="text-black" type="text" name="model" required />
+
+      <label htmlFor="input-mileageInKm" className="text-white">
+        Przebieg w km
+      </label>
+      <input className="text-black" type="number" name="mileageInKm" required />
 
       <label htmlFor="input-price" className="text-white">
         Cena w zł
       </label>
-      <input
-        className="text-black"
-        type="number"
-        id="input-priceInPLN"
-        name="priceInPLN"
-        required
-      />
+      <input className="text-black" type="number" name="priceInPLN" required />
 
       <label htmlFor="input-location" className="text-white">
         Lokalizacja
       </label>
-      <input
-        className="text-black"
-        type="text"
-        id="input-location"
-        name="location"
-        required
-      />
+      <input className="text-black" type="text" name="location" required />
 
       <label htmlFor="input-description" className="text-white">
         Opis
       </label>
-      <input
-        className="text-black"
-        type="text"
-        id="input-description"
-        name="description"
-      />
+      <textarea className="text-black" name="description" rows={10} />
 
       {/* <input type="image" /> */}
       <SubmitButton />
